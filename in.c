15a85e295f432f631986106674329d19e5d7bcce
Laaqshya <laaqshya@gmail.com>
2025-12-23 17:37:55 +0530

    Update in.c

--- in.c
+++ in.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
 int main() {
     char s1[100], s2[100]; 
-    int i, j, p, k = 0,l1,l2;
+    int i, p, l1, l2;
     printf("Enter s1:\n");
     gets(s1);  
     printf("Enter s2:\n");
     gets(s2); 
-    for(i=0;s1[i]!='\0';i++);
-    l1=i;
-    for(i=0;s2[i]!='\0';i++);
-    l2=i;
-    printf("enter the position:");
-    scanf("%d",&p);
-    for(i = l1; i > p - 1; i--)
+    for(i = 0; s1[i] != '\0'; i++);
+    l1 = i;
+    for(i = 0; s2[i] != '\0'; i++);
+    l2 = i;
+    printf("Enter the position: ");
+    scanf("%d", &p);
+    for(i = l1; i >= p - 1; i--) {
         s1[i + l2] = s1[i];
-    for(i = 0; i < l2; i++)
+    }
+    for(i = 0; i < l2; i++) {
         s1[p - 1 + i] = s2[i];
-        s1[i]='\0';
-    printf("inserted string:");
+    }
+    s1[l1 + l2] = '\0';
+    printf("Inserted string:\n");
     puts(s1);
     return 0;
 }
